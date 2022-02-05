75a70c46435a7b34c70da7f5ed0ee25febb66ee9
Walter Barrios <wbarriosvazquez@gmail.com>
2022-02-04 21:49:41 -0300

    configuro la estructura de la tabla Post

--- page/database/migrations/2022_02_05_003327_create_posts_table.php
+++ page/database/migrations/2022_02_05_003327_create_posts_table.php
@@ -15,6 +15,11 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body');
+
             $table->timestamps();
         });
     }
